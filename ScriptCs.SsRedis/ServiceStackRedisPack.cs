529d315a41911a95fcd03c066040f2ce266406ea
filipw <wojcieszyn@gmail.com>
2013-12-09 08:39:42 +0100

    First commit

---
+++ ScriptCs.SsRedis/ServiceStackRedisPack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.SsRedis
+{
+    public class ServiceStackRedisPack : IScriptPack
+    {
+        public void Initialize(IScriptPackSession session)
+        {
+            session.ImportNamespace("ScriptCs.SsRedis");
+            session.ImportNamespace("ServiceStack.Redis");
+            session.ImportNamespace("ServiceStack.Redis.Generic");
+            session.ImportNamespace("ServiceStack.Redis.Messaging");
+            session.ImportNamespace("ServiceStack.Text");
+            session.ImportNamespace("ServiceStack.Text.Json");
+        }
+
+        public IScriptPackContext GetContext()
+        {
+            return new SsRedis();
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
---
+++ ScriptCs.SsRedis/SsRedis.cs
@@ -0,0 +1,30 @@
+﻿using ScriptCs.Contracts;
+using ServiceStack.Redis;
+
+namespace ScriptCs.SsRedis
+{
+    public class SsRedis : IScriptPackContext
+    {
+        public SsRedis()
+        {
+        }
+
+        public IRedisClient Host()
+        {
+            var client = new RedisClient();
+            return client;
+        }
+
+        public IRedisClient Host(string host)
+        {
+            var client = new RedisClient(host);
+            return client;
+        }
+
+        public IRedisClient Host(string[] hosts)
+        {
+            var manager = new PooledRedisClientManager(hosts);
+            return manager.GetClient();
+        }
+    }
+}
